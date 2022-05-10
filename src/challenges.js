c380a79b26bc02620c7ec9779c2b21933167728a
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-05-10 11:34:03 -0300

    requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -23,9 +23,18 @@ function splitSentence(string) {
 splitSentence("go Trybe");
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let ultimo = array[array.length - 1];
+  let primeiro = array[0];
+  let juntos = ultimo + ", " + primeiro;
+  return juntos;
 }
+let nomes1 = ["Lucas", "Cassiano", "Ferraz", "Paolillo"];
+let nomes2 = ["foguete", "não", "tem", "ré"];
+let nomes3 = ["captain", "my", "captain"];
+console.log(concatName(nomes1));
+console.log(concatName(nomes2));
+console.log(concatName(nomes3));
 
 // Desafio 5
 function footballPoints() {
